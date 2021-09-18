13120d0b11c84c9efad8e84033259711b1d147fc
MHOPI98 <bdmhopi@gmail.com>
2021-09-18 14:15:59 +0600

    This is my hacker rank solution

---
+++ Hello World.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    char s[100];
+    cin>>s;
+
+    cout<<"Hello, World!"<<endl;
+    cout<<s<<endl;
+    return 0;
+}
---
+++ Sum and Difference of Two Numbers.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a,b;
+    double c,d;
+    cin>>a>>b>>c>>d;
+    int m,n;
+    double o,p;
+    m=a+b;
+    n=c+d;
+    o=a-c;
+  p=b-d;
+    cout<<m<<" "<<n<<endl;
+    cout<<fixed<<setprecision(1)<<o<<" "<<p<<endl;
+}
